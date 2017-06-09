87eb6a421e0129f5131ecd0ac96d1c357860b90e
eric peng <eric peng>
2017-06-09 10:17:05 +0800

    finishing submodules

---
+++ src/Baseline/ALU Control/ALUControl.v
@@ -0,0 +1,49 @@
+module ALUControler(
+    Op,
+    FuncField,
+    ALUctrl,
+);
+	
+	input  [5:0] ALUOp;
+	input  [5:0] FuncField;
+	output [3:0] ALUctrl;
+
+// TODO  //////////////////////////////////////////////////////////////////////////////////
+wire ADD, SUB, AND, OR, XOR, NOR, SLL, SRL, SRA, SLT;
+reg [3:0] Out;
+
+assign ADD = ((Op == 6'b001000) || (Op == 6'b100011) || (Op == 6'b101011) || ({Op,FuncField} == 12'b000000100000)) ? 1 : 0;
+assign SUB = ((Op == 6'b000100) || ({Op,FuncField} == 12'b000000100010)) ? 1 : 0;
+assign AND = ((Op == 6'b001100) || ({Op,FuncField} == 12'b000000100100)) ? 1 : 0;
+assign OR  = ((Op == 6'b001101) || ({Op,FuncField} == 12'b000000100101)) ? 1 : 0;
+assign XOR = ((Op == 6'b001110) || ({Op,FuncField} == 12'b000000100110)) ? 1 : 0;
+assign NOR = (({Op,FuncField} == 12'b000000100111)) ? 1 : 0;
+assign SLL = (({Op,FuncField} == 12'b000000000000)) ? 1 : 0;
+assign SRL = (({Op,FuncField} == 12'b000000000011)) ? 1 : 0;
+assign SRA = (({Op,FuncField} == 12'b000000000010)) ? 1 : 0;
+assign SLT = ((Op == 6'b001010) || ({Op,FuncField} == 12'b000000101010)) ? 1 : 0;
+
+assign ALUctrl = Out;
+
+always@(*) begin
+	if(ADD)      Out = 4'b0000; 
+	else if(SUB) Out = 4'b0001; 
+	else if(AND) Out = 4'b0010; 
+	else if(OR ) Out = 4'b0011; 
+	else if(XOR) Out = 4'b0101; 
+	else if(NOR) Out = 4'b0110; 
+	else if(SLL) Out = 4'b0111; 
+	else if(SRL) Out = 4'b1000; 
+	else if(SRA) Out = 4'b1001; 
+	else if(SLT) Out = 4'b1100; 
+	else         Out = 4'b1111;
+
+end
+
+
+///////////////////////////////////////////////////////////////////////////////////////////   
+
+endmodule
+
+
+
---
+++ src/Baseline/Control Unit/ControlUnit.v
@@ -0,0 +1,53 @@
+module Control(
+    Op,
+	FuncField,
+    Jump,
+	Jr,
+	RegDst,
+    ALUsrc,
+    MemRead,
+    MemWrite,
+    Branch,
+    MemtoReg,
+    RegWrite,
+    Jal
+);
+	
+	input [5:0] Op;
+	input [5:0] FuncField;
+	output Jump;
+	output Jr;
+	output RegDst;
+	output ALUsrc;
+	output MemRead;
+	output MemWrite;
+	output Branch;
+	output MemtoReg;
+	output RegWrite;
+	output Jal;
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+	reg [9:0] Out;
+	assign {Jump,Jr,RegDst,ALUsrc,MemRead,MemWrite,Branch,MemtoReg,RegWrite,Jal} = Out;
+	wire I_type;
+	assign I_Type = ((Op = 6'b001000) || (Op = 6'b001100) || (Op = 6'b001101) || (Op = 6'b001110) || (Op = 6'b001010)) ? 1 : 0;
+
+	always@ (*) begin
+		if(OP == 6'b000000) begin
+			if(FuncField == 6'b001000) Out = 10'b1100000000;	// Jr
+			else if(FuncField == 6'b001001)	Out = 10'b1110000011;	// Jalr ; if (PC <- rd) Jal = 0;
+			else                 Out = 10'b0010000010;	// r-type
+		end 
+		else if( I_Type )        Out = 10'b0001000010;
+		else if(Op == 6'b000100) Out = 10'b0000001000;	// beq
+		else if(Op == 6'b000010) Out = 10'b1000000000;	// j jump
+		else if(Op == 6'b000011) Out = 10'b1000000011;	// jal
+		else if(Op == 6'b100011) Out = 10'b0001100110;	// lw
+		else if(Op == 6'b101011) Out = 10'b0001010000;	// sw
+		else Out = 0;
+	end
+
+///////////////////////////////////////////////////////////////////////////////////////////   
+
+endmodule
